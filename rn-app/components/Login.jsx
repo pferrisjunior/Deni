633ed692884e937a7e7aaa975547002c26e5c0ad
wjb21301 <wjb21301@gmail.com>
2026-02-06 16:39:40 -0500

    Messing

---
+++ rn-app/components/Login.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import { SafeAreaView } from 'react-native-safe-area-context';
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+export default function Login(){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+}
+return(
+    <text>Hi, my name is Paul Ferris, and Deni is now complete.</text>
+);
