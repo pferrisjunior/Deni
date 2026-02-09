b428eb097a0aa4e170764281019f706f21d9f6dc
wjb21301 <wjb21301@gmail.com>
2026-02-09 14:58:00 -0500

    Added running input validation for passwords. Not for e-mails tho, may come in future.

--- rn-app/components/Register.js
+++ rn-app/components/Register.js
@@ -1,5 +1,5 @@
 //We need to verify passwords
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import {
   View,
@@ -10,22 +10,46 @@ import {
   Image,
 } from "react-native";
 export default function Register(){
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [error, setError] = useState('')
+    useEffect(() => {
+      setError(checkPassword(password,confirmPassword));
+    }, [password,confirmPassword]);
+    const isValid = !error && password && confirmPassword;
     const userRegister = () => {
     console.log("New User: ", email, password);
   };
+  function checkPassword(password, confirmPassword){
+    if (!password || !confirmPassword){
+      return ""
+    }
+    if (password.length < 8){
+      return "Password must have at least 8 characters"
+    }
+    if (!/^[A-Z]/.test(password)){
+      return "Password must start with a capital letter"
+    }
+    if (!/[0-9]/.test(password)){
+      return "Password must contain at least one number."
+    }
+    if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)){
+      return "Password must contain at least one symbol."
+    }
+    if (password !== confirmPassword){
+      return "Passwords must match."
+    }
+    return ""
+
+  }
   return (
     <SafeAreaView style ={{ flex: 1, backgroundColor: '#fbe0b3' }}>
       <View style = {styles.container}>
         <View style = {styles.header}>
-          <Image
-            source={require('../assets/logo.png')}
-            style = {styles.headerImg}
-            />
         <Text style = {styles.title}>Register for Deni</Text>
           <Text style = {styles.subtitle}>
-            Register for deni using an email and Password
+            Register for Deni using an E-mail and Password
           </Text>
       </View>
         <View style = {styles.form}>
@@ -54,9 +78,25 @@ export default function Register(){
               onChangeText={setPassword}
               />
             </View>
-          <TouchableOpacity style = {styles.btn} onPress = {userRegister}>
-              <Text style = {styles.buttonText} > Press here to register! </Text>
-          </TouchableOpacity>  
+          <View style = {styles.input}>
+            <Text style = {styles.inputLabel}> Confirm Password</Text>
+            <TextInput
+              secureTextEntry
+              style = {styles.inputControl}
+              placeholder="**********"
+              placeholderTextColor = "#6b7280"
+              value = {confirmPassword}
+              onChangeText={setConfirmPassword}
+              />
+            </View>
+          {error ? <Text style = {styles.error}>{error}</Text> : null}
+        <TouchableOpacity
+        style={[styles.btn, !isValid && styles.btnDisabled]}
+        onPress={userRegister}
+        disabled={!isValid}
+        >
+          <Text style={styles.btnText}>Press here to register!</Text>
+        </TouchableOpacity>
         </View>
       </View>
     </SafeAreaView>
@@ -72,13 +112,7 @@ const styles = StyleSheet.create({
   },
   header: {
     marginVertical: 36,
-  },
-  headerImg: {
-    width: 300,
-    height: 120,
-    alignSelf: "center",
-    resizeMode: 'contain',
-    marginBottom: 36,
+    alignItems: "center"
   },
   title: {
     alignSelf: "center",
@@ -89,17 +123,12 @@ const styles = StyleSheet.create({
 
   },
   subtitle: {
+    alignself: "center",
     fontSize: 15,
     fontWeight: '500',
     color: '#1e1e1e',
     marginBottom: 6,
 
-  },
-  subtitle: {
-    fontSize: 15,
-    fontWeight: "500",
-    color: "#929292",
-    textAlign: "center",
   },
   form: {
     marginBottom: 24,
@@ -136,6 +165,15 @@ const styles = StyleSheet.create({
   btnText: {
     fontSize: 18,
     fontWeight: "600",
-    color: "#fff",
+    color: "#222",
   },
+  btnDisabled: {
+  opacity: 0.5,
+},
+ error: {
+  color: "red",
+  textAlign: "center",
+  marginBottom: 12,
+  fontSize: 14,
+}, 
 });
