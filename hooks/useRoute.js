331502d0dc90053a541ffd3eeb7e11486eee52ef
wjb21301 <wjb21301@gmail.com>
2026-03-30 17:22:38 -0400

    Routes now working. One ui bug.

--- hooks/useRoute.js
+++ hooks/useRoute.js
@@ -1,26 +1,70 @@
 import { useState } from "react";
 import polyline from "@mapbox/polyline";
-export default function useRoute(){
-    const [routeCoords, setRouteCoords] = useState([]);
-    const [distance, setDistance] = useState(null);
-    const [duration, setDuration] = useState(null);
-    const clearRoute = () => {
-        setRouteCoords([]);
-        setDistance(null);
-        setDuration(null);
-    }
-    const getRoute = async (origin, destination) => {
-        if (!origin){
-            alert("No starting location available.")
-            return;
-        }
+import { auth } from "../lib/firebase"
+export default function useRoute() {
+    const [route, setRoute] = useState(null);
+    const [coords, setCoords] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const getRoute = async (origin, destination, mode) => {
         try {
-            clearRoute;
-            //Add api call when I get render up and running.
-            const res = await fetch(
+            setLoading(true);
+            setError(null);
+            const user = auth.currentUser;
 
-            )
+            if (!user) {
+                throw new Error("User not authenticated");
+            }
+
+            const token = await user.getIdToken();
+            const response = await fetch("https://routes-api-1gfa.onrender.com/route", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                     "Authorization": `Bearer ${token}`,
+                },
+                body: JSON.stringify({
+                    origin,
+                    destination,
+                    mode,
+                }),
+            });
+            if (!response.ok) {
+                const text = await response.text();
+                console.log("SERVER ERROR:", text);
+                throw new Error("Route request failed");
+            }
+            const data = await response.json();
+            const decoded = polyline.decode(data.polyline).map(([lat, lng]) => ({
+                latitude: lat,
+                longitude: lng,
+            }));
+            setRoute(data);
+            setCoords(decoded);
+        } catch (err) {
+            console.error("Route error:", err);
+            setError(err);
+        } finally {
+            setLoading(false);
         }
-    }
 
+    };
+    const clearRoute = () => {
+        setRoute(null);
+        setCoords([]);
+        setError(null);
+    };
+
+    return {
+        route,
+        coords,
+        loading,
+        error,
+        getRoute,
+        clearRoute,
+        distanceText: route?.distance ?? "",
+        durationText: route?.duration ?? "",
+    };
 }
+
+
